0b3a5604346216dd1fb02fa4c1f051e08c80b277
yolomonkey <zan.molnarlm@gmail.com>
2025-11-10 19:03:19 +0100

    database + seed update
    - added hierarchy to seed
    - fixed database relations
    - specified relation aliases

--- server/models/sync.ts
+++ server/models/sync.ts
@@ -3,22 +3,63 @@ import Employee from "./employee.ts";
 import TimeLog from "./timeLog.ts";
 import LeaveRequest from "./leaveRequest.ts";
 
+const isDev = true;
 // EMPLOYEE -> EMPLOYEE (manager relationship)
 Employee.hasMany(Employee,{
-    as:'subordinates',
-    foreignKey: 'managerId'
+    as:'subordinates', // alias for manager.subordinates
+    foreignKey: 'managerId',
 });
 Employee.belongsTo(Employee, {
-    as: 'manager',
+    as: 'manager', // alias for employee.manager
     foreignKey: 'managerId',
 });
 
 // EMPLOYEE -> TIME LOG
+Employee.hasMany(TimeLog, {
+    as: "timeLogs", // epmloyee.timeLogs
+    foreignKey: "employeeId",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+});
+TimeLog.belongsTo(Employee, {
+    foreignKey: "employeeId",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+})
 
+// EMPLOYEE -> LEAVE REQUEST
+Employee.hasMany(LeaveRequest, {
+    as: "leaveRequests", //employee.leaveRequests
+    foreignKey: "employeeId",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+});
+TimeLog.belongsTo(Employee, {
+    foreignKey: "employeeId",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+});
 
+// APPROVER (Manager/Admin) -> LEAVE REQUESTS
+Employee.hasMany(TimeLog, {
+    as: "approvedRequests", // manager.approvedRequests
+    foreignKey: "approvedBy",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+});
+TimeLog.belongsTo(Employee, {
+    as:"approver", // leaveRequest.approver
+    foreignKey: "approvedBy",
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+})
 const syncModels = async () => {
     try {
-        await sequelize.sync({ alter: true });
+        if (isDev){
+            await sequelize.sync({ force: true })
+        } else {
+            await sequelize.sync({ alter: true })
+        }
         console.log("All models synced successfully");
     } catch (err){
         console.error("Error syncing models:", err);
